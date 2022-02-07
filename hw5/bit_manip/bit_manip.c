81027c430977349ae53eafd2e5c5ebd91b30fa18
seanmorton2023 <63743881+seanmorton2023@users.noreply.github.com>
2022-02-06 21:15:46 -0600

    midway through hw5

---
+++ hw5/bit_manip/bit_manip.c
@@ -0,0 +1,11 @@
+
+int main(void) {
+
+unsigned int u1=33, u2=17, u3;
+
+u3 = u1 & u2; // bitwise AND
+u3 = u1 | u2; // bitwise OR
+u3 = u2 << 4; // shift left 4 spaces, or multiply by 2ˆ4 = 16
+u3 = u1 >> 3; // shift right 3 spaces, or divide by 2ˆ3 = 8
+
+}
